e5b06659b3f2904bd108e4ed84cd5ee6f5f96a3f
chrismartinez123 <70671329+chrismartinez123@users.noreply.github.com>
2025-05-25 11:51:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,6 @@ def predict():
 
 # 9. Iniciar la app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
